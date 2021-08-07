5028a3029a316ffa18e236dc69c3e3f4e2beb3c2
pdbrooks <PDBrooks01@outlook.com>
2021-08-07 18:53:58 -0400

    Server set up and ready to roll

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors= require('cors')
 
 
- 
+const app = express()
+
+app.use(bodyParser.json({limit: "30mb", extended: true})) //for images
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 3005
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> app.listen(PORT,()=> console.log(`Server is ready to roll on port: ${PORT}`)))
+.catch((error)=> console.log(error.message))
+
+mongoose.set('useFindAndModify', false)//makes sure no warnings in console
